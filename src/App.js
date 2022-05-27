6a3a429d138f5ceb020011a1eaa1622680ab7153
Alif Aunurrohim <alifaunurrohim@gmail.com>
2022-05-27 16:13:44 +0700

    Implements ReactJS Single Page: (+) add Bootstrap to project (+) add router component to project (+) add Router Dom library (+) add header component (+) add REST API JSON Server to project (+) add Redux component to project (+) add Axios & SweetAlert2 dependencies
    Ongoing process:
    - add CRUD Rest API to project
    - add styling to website

--- src/App.js
+++ src/App.js
@@ -11,40 +11,17 @@ import ContactApps from './views/ContactApps';
 import DetailContact from './views/DetailContact';
 import CategoryContact from './views/CategoryContact';
 
+// components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <>
-    <nav className='navbar navbar-expand-lg navbar-light bg-light shadow-sm'>
-      <div className='container-fluid'>
-
-        <Link to="/" className='navbar-brand'>Contact Apps</Link>
-
-        <button type='button' className='navbar-toggler' data-bs-toggle='collapse' 
-        data-bs-target="#navbarNav" aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle 
-        navigation'>
-          <span className='navbar-toggler-icon'></span>
-        </button>
-
-        <div className='collapse navbar-collapse justify-content-between' id='navbarNav'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <Link to="/" className='nav-link'>All Contact</Link>
-            </li>
-            <li className='nav-item'>
-              <Link to="/detail" className='nav-link'>detail</Link>
-            </li>
-            <li className='nav-item'>
-              <Link to="/category" className='nav-link'>category</Link>
-            </li>
-          </ul>
-        </div>
-
-      </div>
-    </nav>
+      <Navbar/>
       <Routes>
         <Route exact path='/' element={<ContactApps/>} />
-        <Route exact path='/detail' element={<DetailContact/>} />
-        <Route exact path='/category' element={<CategoryContact/>} />
+        <Route exact path='/:category' element={<CategoryContact/>} />
+        {/* <Route exact path='/detail' element={<DetailContact/>} /> */}
       </Routes>
     </>
   );
--- src/views/ContactApps.jsx
+++ src/views/ContactApps.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from 'react-redux';
+
+// action
+import getAllContact from '../redux/action/getAllContact';
+
+// components
+import CardContact from "../components/CardContact";
 
 export default function ContactApps() {
-    return(
-        <p>Homepage</p>
+    const dispatch = useDispatch();
+    const loading = useSelector(state=>state.loadingReducer.loading);
+    const contacts = useSelector(state=>state.contactReducer.allContact);
+
+
+    useEffect(() => {
+        dispatch(getAllContact())
+    }, [])
+
+    return (
+        <>
+            <div className="container mt-2">
+                <div className="row">
+                    {
+                        loading ?
+                        <div className="d-flex.justify-content-center mt-5">
+                            <div className="spinner-border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                        :
+                        contacts.length === 0 ?
+                        <div className="text-center m-2 p-4">Data Not Found</div>
+                        :
+                        contacts.map((contact) => {
+                            return (
+                                <div key={contact.id} className="col-md-3 p-1">
+                                    <CardContact contact={contact}/>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
     )
 }
